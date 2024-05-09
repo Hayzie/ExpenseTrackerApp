80103a378fb86e55aeab5d7e4ff8ac9773c0f612
Hazel Phaho <Hazelphaho@gmail.com>
2024-05-09 18:15:12 +0200

    Update nav.svelte

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -3,9 +3,8 @@
     <a href="http:/app">App</a> -->
     <nav class="flex flex-initial" aria-label="Global">
        
-        <div class="flex">
+        <div class="flex pt-4">
             <a href="/" class="text-sm mx-4 font-semibold leading-6 text-gray-100">Home</a>
-            <a href="/app" class="text-sm mx-4 font-semibold leading-6 text-gray-100">App</a>
             <a href="https://github.com/Hayzie/ExpenseTrackerApp" class="text-sm font-semibold leading-6 text-gray-100">Source code</a>
         </div>
     
